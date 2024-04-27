44d9c1d0b2cfa9d3d61ddcf71d7d4460a7442322
4Sight <116753476+4S1ght@users.noreply.github.com>
2024-04-28 01:46:29 +0200

    [add] Base project files

--- server/lib/config/config.ts
+++ server/lib/config/config.ts
@@ -2,11 +2,17 @@
 
 import fs from 'fs/promises'
 import path from 'path'
+import yaml from 'js-yaml'
 
 // Types ======================================================================
 
 // Code =======================================================================
 
 export default class Config {
+
+    public static async load() {
+
+
+    }
 
 }
---
+++ server/server.ts
@@ -0,0 +1,17 @@
+// Imports ====================================================================
+
+import { effect } from "./lib/util/effect.js"
+
+// Types ======================================================================
+
+// Code =======================================================================
+
+export default class FileflyServer {
+
+    public static async start() {
+
+    }
+
+}
+
+FileflyServer.start()
